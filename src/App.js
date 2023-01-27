43128da05d9d785e94bf84843ac1286ed3ee72d2
Timosky81 <66025394+Timosky81@users.noreply.github.com>
2023-01-27 16:33:58 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Skill from './components/Skill'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Skill />
+      <Portfolio />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
